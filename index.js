b76c84b0f79453f21613a333951dec0410bfb31a
Sang Yoo Kim <sangyookim@ogqcorp.com>
2022-08-12 14:33:18 +0900

    Add dotenv

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
 console.log("Welcome to instaliker")
+require("dotenv").config();
 const p = require('puppeteer');
 
 
+const inputText = async (page, selector, text) => {
+  const input = await page.waitForSelector(selector)
+  return await page.type(selector, text)
+
+}
+
 (async ()=> {
-  const ACCOUNT_USERNAME = "IAMABOT"
+  console.log(process.env)
   const browser = await p.launch({headless: false})
   console.log("Launch Puppeteer Browser")
   const page = await browser.newPage()
   await page.goto(`https://www.instagram.com`)
-  const inputUsernameSelector = 'input[name="username"'
-  const inputUsername = await page.waitForSelector(inputUsernameSelector)
-  console.log(`inputUsername`, inputUsername)
-  await page.type(inputUsernameSelector, ACCOUNT_USERNAME)
+
+  await inputText(page, 'input[name="username"]', process.env.ACCOUNT_USERNAME)
   
   // browser.close()
 })()
